417ea52afb6fe2cadad2af3e90146f5b1df25a90
miguelruiz7 <miguelruizzamora7@gmail.com>
2023-01-29 13:44:09 -0600

    Creando modulo páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { AboutComponent } from './pages/about/about.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
-    AboutComponent,
-    HomeComponent,
     MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
